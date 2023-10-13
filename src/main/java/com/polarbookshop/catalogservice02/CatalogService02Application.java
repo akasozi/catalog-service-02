63bbc59e1c557510056b141c0c15607a1c76cb36
Abu Kasozi <akasozi@Abus-MacBook-Pro.local>
2023-10-13 11:53:49 +0300

    initial commit

---
+++ src/main/java/com/polarbookshop/catalogservice02/CatalogService02Application.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogService02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogService02Application.class, args);
+	}
+
+}
