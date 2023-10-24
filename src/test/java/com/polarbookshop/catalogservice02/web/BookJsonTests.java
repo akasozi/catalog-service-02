3594bacd9707f1229dea7b88d0cbfd5206304367
Abu Kasozi <akasozi@Abus-MacBook-Pro.local>
2023-10-24 18:02:32 +0300

    Modified build.gradle

--- src/test/java/com/polarbookshop/catalogservice02/web/BookJsonTests.java
+++ src/test/java/com/polarbookshop/catalogservice02/web/BookJsonTests.java
@@ -1,13 +1,12 @@
 package com.polarbookshop.catalogservice02.web;
 
+
 import com.polarbookshop.catalogservice02.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @JsonTest
 class BookJsonTests {
@@ -32,13 +31,13 @@ class BookJsonTests {
     @Test
     void testDeserialize() throws Exception {
         var content = """
-                {  
-                  "isbn": "1234567890",
-                  "title": "Title",
-                  "author": "Author",
-                  "price": 9.90
-                }
-                """;
+      {
+        "isbn": "1234567890",
+        "title": "Title",
+        "author": "Author",
+        "price": 9.90
+      }
+      """;
         assertThat(json.parse(content))
                 .usingRecursiveComparison()
                 .isEqualTo(new Book("1234567890", "Title", "Author", 9.90));
